5be50057100480b28d493ae421f8b2ec2c3a270f
Ademir Monteiro <ademirmsfilho@fac.pe.senac.br>
2021-01-15 16:27:40 -0300

    Site logado

---
+++ Site Request Logado/subpages/perfil-IES/configPerfil-IES/configCij/js/scripts.js
@@ -0,0 +1,8 @@
+function adicionar(){
+    let contatos = document.getElementById("contatoextra")
+    let contatos1 = document.getElementById("contatoextra1")
+    let botao1 = document.getElementById("adicionar")
+    contatos.style.visibility = "visible"
+    contatos1.style.visibility = "visible"
+    botao1.style.display = "none"
+}
